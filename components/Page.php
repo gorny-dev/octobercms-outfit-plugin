8440886d31dcaaab50613aeecf0b961d2c1f07b1
michal7515 <michal.gorny75@gmail.com>
2020-07-10 01:20:50 +0200

    Update Page component

--- components/Page.php
+++ components/Page.php
@@ -1,10 +1,10 @@
 <?php
 
-
 namespace Codeclutch\Outfit\Components;
 
 use Cms\Classes\ComponentBase;
 use Lang;
+use Codeclutch\Outfit\Models\Page as PageModel;
 
 class Page extends ComponentBase
 {
@@ -14,5 +14,16 @@ class Page extends ComponentBase
             'name' => Lang::get('codeclutch.outfit::lang.plugin.page.name'),
             'description' => Lang::get('codeclutch.outfit::lang.plugin.page.description'),
         ];
+    }
+
+    public function getPagesList()
+    {
+        return PageModel::all();
+    }
+
+    public function onRun()
+    {
+        $slug = $this->param('slug');
+        $this->page['page'] = PageModel::where('slug', $slug)->first();
     }
 }
